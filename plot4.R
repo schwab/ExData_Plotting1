b5c344f5438560a819dcb0bf7d1b08f5c6859ef5
Michael Schwab <mcstar@gmail.com>
2016-05-01 13:31:35 -0500

    completed plot 4

--- plot4.R
+++ plot4.R
@@ -45,12 +45,20 @@ if(!file.exists(path))
   df <- read.csv(path)
 }
 
-# Generate plot3
+# Generate plot4
+
+png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
-png("plot3.png", width=480, height=480)
+
+plot(df$DateFormat, as.numeric(df$Global_active_power), type="l", xlab="", ylab="Global Active Power (kilowatts)" )
+
+plot(df$DateFormat, as.numeric(df$Voltage), type="l", xlab="datetime", ylab="Voltage")
+
 plot(df$DateFormat, as.numeric(df$Sub_metering_1), type="l",  xlab="", ylab="Energy sub metering")
 lines(df$DateFormat, as.numeric(df$Sub_metering_2), col="red")
 lines(df$DateFormat, as.numeric(df$Sub_metering_3), col="blue")
 legend("topright",9.5, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","blue","red"))
+
+plot(df$DateFormat, as.numeric(df$Global_reactive_power), type="l", xlab="datetime", ylab="Global Active Power (kilowatts)" )
 # Turn off device
 dev.off()
